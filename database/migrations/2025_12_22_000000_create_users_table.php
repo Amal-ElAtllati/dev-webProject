73a8aa221a03d13c21c0feba6b636ac19093b60c
Amal-ElAtllati <elatllati.amal@etu.uae.ac.ma>
2026-01-01 22:09:07 +0100

    Modification migration users

--- database/migrations/2025_12_22_000000_create_users_table.php
+++ database/migrations/2025_12_22_000000_create_users_table.php
@@ -11,6 +11,7 @@ return new class extends Migration {
             $table->string('name');
             $table->string('email')->unique();
             $table->string('password');
+            // $table->string('role')->default('user'); // guest, user, tech, admin
             $table->timestamps();
         });
     }
